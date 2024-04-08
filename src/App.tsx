60ab0f906bacf6b7d966bb6d7b971d60ed19624a
76marin76 <76marin76@gmail.com>
2024-04-08 23:09:16 +0200

    added all constants for the landing page; initializing header and styling of it for the large screens;

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Button from "./components/Button.jsx";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1>hello world</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-20" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
